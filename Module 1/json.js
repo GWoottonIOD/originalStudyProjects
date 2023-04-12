81aad6a91bc8807837096aaa653c049a6e78db0f
GWraath <gar.wootton@gmail.com>
2023-04-12 13:20:25 +1200

    Moved items to module 1 folder

---
+++ Module 1/json.js
@@ -0,0 +1,10 @@
+let carmanufactuer_object = {
+    "Name":"Ferrari",
+    "Country":"Italy",
+    "Formula 1":"Yes",
+    "Best selling":"458 Italia",
+}
+
+console.log(carmanufactuer_object.Name)
+
+console.log(carmanufactuer_object["Best selling"])
