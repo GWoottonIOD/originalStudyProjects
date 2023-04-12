29ff95c66e949c1d1c8e59f3f064e793b2094ba0
GWraath <gar.wootton@gmail.com>
2023-04-12 13:47:56 +1200

    Mod 6 & sorted items added

--- Module 6/src/index.js
+++ Module 6/src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom/client'; //ReactDOM importing
+// import PortalApp from './PortalApp';
+ import CalcEmoji from './CalcEmoji';
+//import './index.css'; //CSS importing
+import Calculator from './Calculator'; //what functions are imported from which App
+import Emoji from './Emoji';
+import Exercise1 from './Exercise2';
+import App from './Appex1';
+// import OnHover from './onHover';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  
+//   const root = ReactDOM.createRoot(document.getElementById('root')); 
+//   root.render(<MemoComp/>);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const root = ReactDOM.createRoot(document.getElementById('root')); 
+    root.render(<CalcEmoji/>);
+
+//   export default React.memo(MemoComp)
